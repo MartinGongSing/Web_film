eb36799dfd5ce0c675fa028be8c9192dc4b23600
MartinGongSing <47624744+MartinGongSing@users.noreply.github.com>
2021-02-23 14:16:55 +0100

    form

--- site/src/Controller/TitleController.php
+++ site/src/Controller/TitleController.php
@@ -7,6 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Film;
 use App\Repository\FilmRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class TitleController extends AbstractController
 {
@@ -41,5 +46,73 @@ class TitleController extends AbstractController
         return $this->render('title/theme.html.twig', [
             'title' => "Theme Page"
         ]);
+    }
+
+    /**
+     * @Route("/title/new", name="title_create")
+     */
+    public function create(Request $request) {
+        $film = new Film();
+
+        $form = $this->createFormBuilder($film)
+        // rajouter les attributs (ce qui est dans les cases en grisé)
+                    ->add('Number', NumberType::class, [
+                        'attr' => [
+                            'placeholder' => "Numéro de classement",
+                        ]
+                    ])            
+                    ->add('Title', TextType::class,[
+                        'attr' => [
+                            'placeholder' => "Titre du film",
+                        ]
+                    ])
+                    ->add('Theme', ChoiceType::class,[
+                        'choices' => [ 
+                            'Acteurs Français' => 'Acteurs Français',
+                            'Acteurs Etrangers' => 'Acteurs Etrangers',
+                            'Musical' => 'Musical',
+                            'Documentaire' => 'Documentaire',
+                            'Western' => 'Western',
+                            'AUTRE' => 'Autre',
+                        ]
+                    ],
+                    )
+                    ->add('Actor', TextType::class,[
+                        'attr' => [
+                            'placeholder' => "Acteur principal",
+                        ]
+                    ])
+                    ->add('Year', NumberType::class, [
+                        'attr' => [
+                            'placeholder' => "Année de sortie",
+                        ]
+                    ])
+                    ->add('Prop', TextType::class,[
+                        'attr' => [
+                            'placeholder' => "Propriétaire du film",
+                        ]
+                    ])
+                    ->add('Info', TextareaType::class,[
+                        'attr' => [
+                            'placeholder' => "Information sur le film",
+                        ]
+                    ])
+                    ->getForm();
+        $form->handleRequest($request); 
+
+        // dump($film);
+        if($form->isSubmitted() && $form->isValid()){
+
+            // $manager->persist($film);
+            // $manager->flush();
+
+            return $this->redirectToRoute('title');
+        }
+
+
+        return $this->render('title/create.html.twig', [
+            'formFilm' => $form ->createView()
+        ]);
+
     }
 }
